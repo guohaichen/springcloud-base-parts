df4cc10237b8b0c1222f4d2e08e31e2f3f14a1c8
guohai <1299117865@qq.com>
2022-05-31 20:02:00 +0800

    nacos集群配置，本地应先启动nacos集群服务，80端口为统一由nginx去代理

--- order-service/src/main/java/com/chen/order/OrderApplication.java
+++ order-service/src/main/java/com/chen/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.chen.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
