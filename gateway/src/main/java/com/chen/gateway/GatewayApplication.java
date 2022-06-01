1d64fb173d2941740c1197d040c4691629d0f3ae
guohai <1299117865@qq.com>
2022-06-01 14:01:16 +0800

    gateway 网关组件的加入

---
+++ gateway/src/main/java/com/chen/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.chen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cgh
+ * @create 2022-06-01 11:03
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
